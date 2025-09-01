9f5bb0e21889365ad18a0f401932677a0fda65b7
lucaswokf <lucasinfu@gmail.com>
2025-09-01 11:16:11 -0300

    Add files via upload

---
+++ trab kauan/trab-do-kauan-25/README.md
@@ -0,0 +1,1 @@
+# trab-do-kauan-25
---
+++ trab kauan/trab-do-kauan-25/models/produto_models.py
@@ -0,0 +1,17 @@
+from db import db
+
+class Produto(db.Model):
+    __tablename__ = 'produto'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(80), nullable=False)
+    categoria = db.Column(db.String(80), nullable=False)
+    preco = db.Column(db.Integer, nullable=False)
+
+    def json(self):
+        return {
+            'id': self.id,
+            'nome': self.nome,
+            'categoria': self.categoria,
+            'preco': self.preco
+        }
